d05c35258f34f88674b6f85646aa5e1c6994844f
Walter Lensinas <wlensinas@gmail.com>
2019-01-21 12:01:48 -0300

    Agregando la clase question

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
 
 
 const modules =  [
     MatToolbarModule,
+    MatIconModule,
     MatIconModule
 ];
 
--- src/app/question/question-detail.component.ts
+++ src/app/question/question-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Question } from './question.model';
 
 @Component({
     selector: 'app-question-detail',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class QuestionDetailComponent {
-
+    question: Question = new Question(
+        'Nueva pregunta',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia mi vitae nisi rutrum lobortis. Nam commodo dui eget tellus auctor, in hendrerit ante feugiat. Nullam luctus porta ipsum, in bibendum dui facilisis non. Nulla vestibulum lacinia ultrices. Nunc ac ultricies nibh. Pellentesque maximus lorem eget bibendum volutpat. Maecenas ullamcorper lacus diam, id sollicitudin nibh sagittis vel. Nulla mattis dui sit amet erat auctor ullamcorper.',
+        new Date(),
+        'devicon-android-plain'
+    );
 }
