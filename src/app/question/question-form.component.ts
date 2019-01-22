aa2e7afd520175dd3b309516aa434ed906667865
Walter Lensinas <wlensinas@gmail.com>
2019-01-22 18:06:19 -0300

    agregando los controles al formulario

--- src/app/question/question-form.component.ts
+++ src/app/question/question-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Question } from './question.model';
 
 @Component({
     selector: 'app-question-form',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
 })
 
 export class QuestionFormComponent {
+    onSubmit(form: NgForm) {
+        const q = new Question(
+            form.value.title,
+            form.value.description
+        );
 
+        console.log(q);
+    }
 }
