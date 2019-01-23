029aeb661bb6e4b3ee709cedd53fd33fa22d6f61
Walter Lensinas <wlensinas@gmail.com>
2019-01-23 12:15:08 -0300

    agregando las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuestionListComponent } from './question/question-list.component';
+import { SigninScreenComponent } from './auth/signin-screen.component';
+import { SignupScreenComponent } from './auth/signup-screen.component';
 
-const routes: Routes = [];
+// APP_ROUTES
+const routes: Routes = [
+{ path: '', component: QuestionListComponent, pathMatch: 'full' }, 
+{ path: 'signin', component: SigninScreenComponent },
+{ path: 'signup', component: SignupScreenComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
