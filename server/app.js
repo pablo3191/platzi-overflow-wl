c054e0ced0f2fe900e2125f9828bb8369141009d
Walter Lensinas <wlensinas@gmail.com>
2019-01-24 11:10:33 -0300

    instalando express y realizando el setup para ejecutar server y client

---
+++ server/app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hola desde express')
+})
+
+export default app
--- server/index.js
+++ server/index.js
@@ -1,15 +1,9 @@
 import http from 'http'
 import Debug from 'debug'
+import app from './app'
 
 const PORT = 3000
 const debug = new Debug('example-app:root')
-
-const app = http.createServer((req, res) => {
-    debug('new request')
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.write('Hola Mundo')
-    res.end()
-})
 
 app.listen(PORT, ()=> {
     debug(`Server running at port: ${PORT}`)
