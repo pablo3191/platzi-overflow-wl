c447d28946a58d2b6e80e3083f1c29aa7f14a054
Walter Lensinas <wlensinas@gmail.com>
2019-02-26 12:44:09 -0300

    Creando el modelo de usuario

--- server/model/answer.js
+++ server/model/answer.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
 
-const AnswerSchema = Schema({
+const AnswerSchema = new Schema({
     description: { type: String, required: true },
     createdAt: { type: Date, default: Date.now, required: true }
 })
